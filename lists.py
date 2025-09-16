02092bc006948412ffbbd30ada659ccfafaadf91
ziora-lawrence <danieliwuji28@gmail.com>
2025-09-16 21:32:09 +0100

    feat: list assignment

---
+++ lists.py
@@ -0,0 +1,22 @@
+#my_list = ["apple", "banana", "cherry"] 
+#print(my_list[1])  # Output: banana
+#my_list = ["apple", "banana", "banana" "cherry"]
+#print(my_list[2])  # Output: bananacherry
+
+#thislist = ['apple', 'banana', 'cherry']
+#print(len(thislist))
+
+#thislist = ["apple", "banana", "cherry"]
+#print(thislist[-1])  # Output: cherry
+
+#thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+#print(thislist[1:4])  # Output: ['banana' 'cherry', 'orange',]
+
+#mylist = ['apple', 'banana', 'cherry']
+#mylist[0] = 'kiwi'
+#print(mylist[1])  # Output: banana
+
+mylist = ['apple', 'banana', 'cherry']
+mylist[1:2] = ['kiwi', 'mango']
+print(mylist)
+
